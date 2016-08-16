2d9cbc70a1a0e47a5ef1bfe4a276c749c543e586
mcquam <murray.mcquarrie@gmail.com>
2016-08-16 16:44:04 +0100

    dynamic test

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
 
 # use decorators to link the function to a url
 @app.route("/")
@@ -13,7 +15,35 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, Wirld!"
 
+@app.route("/test/<search_query>")
+# define the view using a function, which returns a string
+def search(search_query):
+    return search_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print (value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print (value + 1)
+    return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print (value)
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael" :
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
